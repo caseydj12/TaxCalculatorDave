b98aac986e0788a570abd6e5e49902f2630611e2
David Casey <caseydj@dukes.jmu.edu>
2024-04-10 13:47:36 -0400

    add missing returns in comment headers

--- TaxCalc/BusinessLogic/ITaxCalculatorBL.cs
+++ TaxCalc/BusinessLogic/ITaxCalculatorBL.cs
@@ -11,31 +11,31 @@
         /// Tax Band C: 20000+ 40 %
         /// </summary>
         /// <param name="grossAnnualIncome"></param>
-        /// <returns></returns>
+        /// <returns>Annual Tax Paid</returns>
         public double CalculateAnnualTaxPaid(double grossAnnualIncome);
         /// <summary>
         /// Calculates the Monthly Gross Salary for Gross Annual income passed.
         /// </summary>
         /// <param name="grossAnnualIncome"></param>
-        /// <returns></returns>
+        /// <returns>Monthly Gross Salary</returns>
         public double CalculateMonthlyGrossSalary(double grossAnnualIncome);
         /// <summary>
         /// Calculates the Annual Net Salary after taxes for Gross Annual income passed.
         /// </summary>
         /// <param name="grossAnnualIncome"></param>
-        /// <returns></returns>
+        /// <returns>Annual Net Salary</returns>
         public double CalculateAnnualNetSalary(double grossAnnualIncome);
         /// <summary>
         /// Calculates the Monthly Net Salary after taxes for Gross Annual income passed.
         /// </summary>
         /// <param name="grossAnnualIncome"></param>
-        /// <returns></returns>
+        /// <returns>Monthly Net Salary</returns>
         public double CalculateMonthlyNetSalary(double grossAnnualIncome);
         /// <summary>
         /// Calculates the Monthly Taxes paid for Gross Annual income passed.
         /// </summary>
         /// <param name="grossAnnualIncome"></param>
-        /// <returns></returns>
+        /// <returns>Monthly Tax Paid</returns>
         public double CalculateMonthlyTaxPaid(double grossAnnualIncome);
     }
 }
